2f9926a47d58a73d9de85f99e758ce08538a05b3
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-07-04 17:43:54 -0300

    criando página de erro

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,41 @@
+import './NotFoundPage.css';
+
+import Banner from "../../components/Banner/Banner"
+import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header"
+
+import { useContext, useEffect } from "react";
+import { BannerContext } from "../../contexts/BannerContext/BannerContext";
+
+import notFound from "../../assets/notFound.png";
+
+
+
 export const NotFoundPage = () => {
+  const {setBanner} = useContext(BannerContext);
+
+  const setNotFoundBanner = () => {
+    const newBanner = {
+      quote: 'Vixi não encontramos essa página',
+      title: 'Opsssss'
+    };
+    setBanner(newBanner)
+  };
+
+  useEffect(() => {setNotFoundBanner()}, [])
   return (
-      <>
-          Page Not Found
-      </>
+      <div className="notFound-page-Wrapper">
+        <Header/>
+        <Banner/>
+        <div className="notFound-wrapper">
+          <div className="notFound-message-wrapper">
+            	<h3>Página não encontrada.</h3>
+          </div>  
+          <div className="notFound-img-wrapper">
+              <img src={notFound} alt="Not Found" />
+          </div>
+        </div>
+        <Footer/>
+      </div>
   )
 }
